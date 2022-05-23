2e5ad14e552d023cd7659a60767fc6eb07dbd33e
Carl <cboettig@gmail.com>
2022-05-23 20:14:38 +0000

    Fix include_horizon:
    - only compute horizon if not defined. (as time - start_time)
    - only compute start_time if not defined (as min(time) )
    - drop 'interval' from data (not used, and not really information about the key)

--- R/include_horizon.R
+++ R/include_horizon.R
@@ -1,29 +1,34 @@
 #' include_horizon
 #' 
+#' Adds horizon if not present, defined as `time - start_time` with a group
+#' defined by any of: "target_id", "model_id", "pub_time", "variable", "site_id".
+#' Will also add `start_time` if not present, defined as `time - min(time)`
 #' @param df data.frame containing an efi-compliant forecast or score
 #' @param allow_difftime should `horizon` and `interval` be expressed as integers (in seconds)
 #' (default) or as native difftime (compatible in R but not in serializations like csv or parquet)
 #' @export
 include_horizon <- function(df, 
-                            allow_difftime = getOption("neon4cast.allow_difftime", FALSE)){
-  
-  interval <- df %>%
-    group_by(across(any_of(c("target_id", "model_id", "pub_time", 
-                             "variable", "site_id")))) %>% 
-    summarise(interval = min(time - dplyr::lag(time), na.rm=TRUE),
-              start_time = min(time), #- interval,
-              .groups = "drop")
-  
+                            allow_difftime = 
+                              getOption("neon4cast.allow_difftime",
+                                        FALSE)){
+  if (!"start_time" %in% colnames(df)) {
+    interval <- df %>%
+      group_by(across(any_of(c("target_id", "model_id", "pub_time", 
+                               "variable", "site_id")))) %>% 
+      summarise(#interval = min(time - dplyr::lag(time), na.rm=TRUE),
+                start_time = min(time), #- interval,
+                .groups = "drop")
+  }
   ## add columns for start_time and horizon
-  df <- df %>% 
-    left_join(interval,
-              #by = c("target_id", "model_id", "pub_time", "site_id", "variable")
-              ) %>% 
-    mutate(horizon = time - start_time)
-  
+  if(!"horizon" %in% colnames(df)) {
+    df <- df %>%
+      left_join(interval) %>% 
+      mutate(horizon = time - start_time)
+  }
   if(!allow_difftime){
-    df <- df %>% mutate(horizon = as.numeric(lubridate::as.duration(horizon), units="seconds"),
-                        interval = as.numeric(lubridate::as.duration(interval), units="seconds"))
+    df <- df %>% 
+      mutate(horizon = as.numeric(lubridate::as.duration(horizon),
+                                  units="seconds"))
   }
   df
 }
