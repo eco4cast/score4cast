a6e9d6c62704ec887976f35386de5184b1913a9e
Carl <cboettig@gmail.com>
2022-08-18 03:39:36 +0000

    do not assume pub_time

--- R/legacy-format-support.R
+++ R/legacy-format-support.R
@@ -18,7 +18,7 @@ map_old_format <- function(df) {
   }
   
   
-  if (!("start_time" %in% colnames(df))) {
+  if ("pub_time" %in% colnames(df) &  !("start_time" %in% colnames(df))) {
     df <- df |> 
       dplyr::mutate(start_time = lubridate::as_datetime(pub_time))
   }
