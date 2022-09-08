da9da41952f33c2c03fb8a07def08a1494bf5dcd
Carl <cboettig@gmail.com>
2022-09-08 20:58:24 +0000

    update legacy mapper to use new time names

--- R/legacy-format-support.R
+++ R/legacy-format-support.R
@@ -1,4 +1,4 @@
-map_old_format <- function(df) {
+map_old_format <- function(df, filename=NULL) {
   if ("ensemble" %in% colnames(df)) {
     df <- df |>
       dplyr::mutate(family = "sample") |> 
@@ -25,12 +25,40 @@ map_old_format <- function(df) {
                                                              mu="mean",
                                                              sigma="sd"))
   }
+
+  ##
+  if ("pub_time" %in% colnames(fc)) {
+    df <- df |> rename(reference_datetime = pub_time)
+  }
+  if ("start_time" %in% colnames(fc)) {
+    df <- df |> rename(reference_datetime = start_time)
+  }
+  if ("start_time" %in% colnames(fc)) {
+    df  <- df |> select(-start_time)
+  }
+  if("time" %in% colnames(fc)) {
+    df <- df |> rename(datetime = time)
+  }
+
   
-  if ("pub_time" %in% colnames(df) &  !("start_time" %in% colnames(df))) {
-    df <- df |> 
-      dplyr::mutate(reference_datetime = lubridate::as_datetime(pub_time))
+  if ("filename" %in% colnames(df)) {
+    filename <- df |> pull(filename)
+    df <- df |> select(-filename)
+  }
+  
+  if(!is.null(filename)) {
+    pattern <- 
+      "(\\w+)\\-(\\d{4}\\-\\d{2}\\-\\d{2})\\-(\\w+)\\.(csv)?(\\.gz)?(nc)?"
+    x <- basename(filename)
+    df <- df %>% 
+      mutate(target_id = gsub(pattern, "\\1", x),
+             reference_datetime =
+               lubridate::as_datetime(gsub(pattern, "\\2", x)),
+             model_id = gsub(pattern, "\\3", x))
   }
   df
+  
 }
 
-globalVariables(c("ensemble", "pub_time"), package="score4cast")
+globalVariables(c("ensemble", "pub_time", "start_time", "filename"),
+                package="score4cast")
